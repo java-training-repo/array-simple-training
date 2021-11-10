34ce2892b596e32e8dfb8076ce50f6fd5c2f6a81
Zhenya2809 <Zhenya2809@ukr.net>
2021-11-10 23:54:36 +0200

    commit homework

--- src/main/java/org/java/practice/BinarySearch.java
+++ src/main/java/org/java/practice/BinarySearch.java
@@ -7,11 +7,26 @@ public class BinarySearch {
      * Please use the algorithm of binary search.
      *
      * @param numbers already sorted array.
-     * @param num the number which you should find.
+     * @param num     the number which you should find.
      * @return the index of the number if it presents in the array or -1 if not.
      */
     public int getIndexOfElement(int[] numbers, int num) {
-        return -1;
+        int first = 0;
+        int last = numbers.length-1;
+        int position=0;
+        position = (first + last) / 2;
+        while ((numbers[position] != num) && (first <= last)) {
+            if (numbers[position] > num) {
+                last = position - 1;
+            } else {
+                first = position + 1;
+            }
+            position = (first + last) / 2;
+        }
+        if (first <= last) {
+            return position;
+        }else {
+            return -1;}
     }
 
 }
